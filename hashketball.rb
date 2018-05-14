b2be7b7271bd8f30ec6ce20818ec5bfeb9676aca
Michael Ballard <ballard.michael9@gmail.com>
2018-05-14 21:30:37 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -155,7 +155,7 @@ def player_numbers(team_name)
       team_data[:players].each {|player, player_data| numbers << player_data[:number]}
     end
   end
-  numbers.skip_before_filter
+  numbers.sort
 end
 
 def player_stats(player_name)
